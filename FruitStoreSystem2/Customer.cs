64f1ce8368d3d06e0a2b0bf21e08a7e900f58ba7
Sakonporn Noree <juckjick14@gmail.com>
2016-10-19 12:42:07 +0700

    add customer class & attributes

---
+++ FruitStoreSystem2/Customer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FruitStoreSystem2
+{
+    public class Customer
+    {
+        private string name;
+        private string surname;
+        private string phone;
+        private string address;
+        private ReserveOrders ROrder;
+
+        public Customer(string n, string s, string p, string a, ReserveOrders ROrder)
+        {
+            this.name = n;
+            this.surname = s;
+            this.phone = p;
+            this.address = a;
+            this.ROrder = ROrder;
+        }
+    }
+}

909719d87cf9534fa0c615a98d1971cae405e3fa
Chanon-SVN <chanon@kmi.tl>
2016-10-19 16:19:03 +0700

    DB connection success

--- FruitStoreSystem2/Home.aspx.cs
+++ FruitStoreSystem2/Home.aspx.cs
@@ -11,12 +11,27 @@ namespace FruitStoreSystem2
 {
     public partial class Home : System.Web.UI.Page
     {
+        MySql.Data.MySqlClient.MySqlConnection conn;
+        string myConnectionString;
         Entities en = new Entities();
         string connect = ConfigurationManager.ConnectionStrings["con"].ConnectionString;
         protected void Page_Load(object sender, EventArgs e)
         {
-            MySqlConnection con = new MySqlConnection(connect);
-            con.Open();
+            string myConnectionString = "server=fruitstore.cloudapp.net;uid=root;" +
+    "pwd=BSDnewgeneration;database=fruitstore;";
+
+            try
+            {
+                conn = new MySql.Data.MySqlClient.MySqlConnection();
+                conn.ConnectionString = myConnectionString;
+                conn.Open();
+            }
+            catch (MySql.Data.MySqlClient.MySqlException ex)
+            {
+                Console.WriteLine(ex);
+            }
+            //MySqlConnection con = new MySqlConnection(connect);
+            //con.Open();
         }
     }
 }

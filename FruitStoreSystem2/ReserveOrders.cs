de0c014018f7152065285ada4419faaf19fdb92c
Sakonporn Noree <juckjick14@gmail.com>
2016-10-19 12:32:04 +0700

    add reserveOrders class & attributes

---
+++ FruitStoreSystem2/ReserveOrders.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FruitStoreSystem2
+{
+    public class ReserveOrders
+    {
+        private DateTime reserveDate;
+        private DateTime reserveInterval;
+        private Fruit f;
+        private ReserveItems RItems;
+        
+        public ReserveOrders(DateTime reserveDate, DateTime reserveInterval, int n, string g, string status, Fruit f)
+        {
+            this.reserveDate = reserveDate;
+            this.reserveInterval = reserveInterval;
+            this.RItems = new ReserveItems(n,g,status,f);
+        }
+    }
+}

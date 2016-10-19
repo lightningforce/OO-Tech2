b78631a024760f29ef12b4cba0eabdfdbad86f68
Sakonporn Noree <juckjick14@gmail.com>
2016-10-19 12:07:43 +0700

    add Fruit class & attributes

---
+++ FruitStoreSystem2/Fruit.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FruitStoreSystem2
+{
+    public class Fruit
+    {
+        private string type;
+        private string seeds;
+        private FruitInStock fis;
+
+        public Fruit(string t, string s, FruitInStock fs)
+        {
+            this.type = t;
+            this.seeds = s;
+            this.fis = fs;
+        }
+
+    }
+
+    
+}

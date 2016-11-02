2b9e05c65014215badccf29f32a09d148049974f
lightningforce <sorth_27@hotmail.com>
2016-11-02 15:11:32 +0700

    edit name content place holder

--- FruitStoreSystem2/Home.aspx.designer.cs
+++ FruitStoreSystem2/Home.aspx.designer.cs
@@ -31,6 +31,15 @@ namespace FruitStoreSystem2 {
         protected global::System.Web.UI.WebControls.LinkButton btnSearch;
         
         /// <summary>
+        /// gvFruit control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView gvFruit;
+        
+        /// <summary>
         /// gvReserveOrder control.
         /// </summary>
         /// <remarks>

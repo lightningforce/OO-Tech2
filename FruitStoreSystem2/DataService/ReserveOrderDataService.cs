431eac8aaadd441793fab65d8641586ae430b8ec
lightningforce <sorth_27@hotmail.com>
2016-11-03 13:25:23 +0700

    add search function

--- FruitStoreSystem2/DataService/ReserveOrderDataService.cs
+++ FruitStoreSystem2/DataService/ReserveOrderDataService.cs
@@ -33,5 +33,30 @@ namespace FruitStoreSystem2
             }
             return dt;
         }
+        public DataTable searchReserveOrderData(string cusFullName)
+        {
+            DataTable dt = new DataTable();
+            StringBuilder strQuery = new StringBuilder();
+            strQuery.Append("select ");
+            strQuery.Append("r.reserveID");
+            strQuery.Append(",c.cusFName + ' ' + c.cusLName as cus_fullname");
+            strQuery.Append(",r.reserveDate");
+            strQuery.Append(",r.receiveDate");
+            strQuery.Append(",r.status ");
+            strQuery.Append("from ReserveOrder r ");
+            strQuery.Append("inner join Customer c ");
+            strQuery.Append("on r.cusID = c.cusID ");
+            strQuery.Append("where c.cusFname + ' ' + c.cusLName like @cusFullName");
+            using (DataAccess dac = new DataAccess())
+            {
+                dac.Open(Provider.MSSQL);
+                DbCommand cmd = dac.CreateCommand(strQuery.ToString());
+                cmd.CommandType = CommandType.Text;
+                cmd.Parameters.Add(dac.CreateParameter("@cusFullName", "%" + cusFullName + "%"));
+                DbDataAdapter da = dac.CreateDataAdapter(cmd);
+                da.Fill(dt);
+            }
+            return dt;
+        }
     }
 }
--- FruitStoreSystem2/Home.aspx.designer.cs
+++ FruitStoreSystem2/Home.aspx.designer.cs
@@ -31,15 +31,6 @@ namespace FruitStoreSystem2 {
         protected global::System.Web.UI.WebControls.LinkButton btnSearch;
         
         /// <summary>
-        /// gvFruit control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.GridView gvFruit;
-        
-        /// <summary>
         /// gvReserveOrder control.
         /// </summary>
         /// <remarks>
